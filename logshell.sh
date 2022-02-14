33684583dc7983b50c2e8cbe267c55722abd2076
unknown <eric@getmee.au>
2022-02-14 22:52:40 +1100

    logshell update

--- logshell.sh
+++ logshell.sh
@@ -1,1 +1,1 @@
-awk -v d1="$(date --date="-5 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep dhclient* > ./"$(date +"%Y_%b_%d-%I_%M_%S_%p").log"
+sudo awk -v d1="$(date --date="-5 min" "+%b %_d %H:%M")" -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 || $0 ~ d2' /var/log/messages | grep dhclient* > ./"$(date +"%Y_%b_%d-%I_%M_%S_%p").log"
